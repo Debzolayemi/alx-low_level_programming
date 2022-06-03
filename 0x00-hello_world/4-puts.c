916b9d832ec5c71f9b0ee9ab6e8c8137d0faedf3
Olayemi <popooladeborah28@gmail.com>
2022-06-03 01:20:32 -0700

    using puts3

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,11 @@
-/*
- * file: 4-puts.c
- * Auth: Olayemi
- */
-
-#include <stdio.h>
-
-/*
+#include<stdio.h>
+/**
  * main - Prints "Programming is like building a multilingual puzzle, followed by a new line."
- *
+ * 
  * Return: Always 0.
  */
 int main(void)
 {
 	puts ("\"Programming is like building a multilingual puzzle");
-	reurn (0);
+	return (0);
 }
