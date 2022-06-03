68fbdeeef3f9e24bc5cffe11c06c675ef6ef58d0
Olayemi <popooladeborah28@gmail.com>
2022-06-03 01:07:18 -0700

    using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+#!/bin/bash
+/*
+ * file: 4-puts.c
+ * Auth: Olayemi
+ */
+
+#include <stdio.h>
+
+/*
+ * main - Prints "Programming is like building a multilingual puzzle, followed by a new line."
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	puts ("\"Programming is like building a multilingual puzzle");
+	reurn (0);
+}
