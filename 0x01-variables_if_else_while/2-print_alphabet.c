938f1b91046ce49d4bf508d6c0f2d55b7b8b9e5a
Olayemi <popooladeborah28@gmail.com>
2022-06-03 08:06:56 -0700

    Print alphabets2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,12 +4,12 @@
  *
  * Return: Always 0
  */
-int main()
+int main(void)
 {
 	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
 	int i;
 
-	for (i = 0 i < 26; i++)
+	for (i = 0; i < 26; i++)
 	{
 		putchar(alp[i]);
 	}
